861e2061f888e1d98ce092cd9d71772008c9c282
crypt0grapher <7blockchains@gmail.com>
2023-05-08 21:34:03 +0700

    build: simplify presale script

--- tasks/presale.ts
+++ tasks/presale.ts
@@ -1,10 +1,8 @@
-import { WEEK, discountedPresalePriceNFT, presaleMaxSupply } from '@config';
+import { presaleMaxSupply } from '@config';
 import { SafeNFT } from '@contractTypes/contracts';
-import { displayDiscountedPresalePriceNFT, info, networkInfo, success } from '@utils/output.helper';
+import { info, networkInfo, success } from '@utils/output.helper';
 import { task, types } from 'hardhat/config';
-import moment from 'moment';
 
-const timeStampFormat = 'DD.MM.YYYY HH:mm:ss?';
 export default task('presale', 'gets and sets presale date to now or other date')
   .addOptionalParam(
     'test',
@@ -13,32 +11,12 @@ export default task('presale', 'gets and sets presale date to now or other date'
     types.boolean,
   )
   .addOptionalParam('price', 'update the discounted price table and maxSupply per week', false, types.boolean)
-  .addOptionalParam('setdate', 'update the startDate', false, types.boolean)
-  .addOptionalParam(
-    'date',
-    `presale date to set in format ${timeStampFormat}`,
-    moment().format(timeStampFormat),
-    types.string,
-  )
-  .setAction(async ({ test, price, setdate, date }, hre) => {
+  .setAction(async ({ test, price }, hre) => {
     await networkInfo(hre, info);
 
     const nftContract = await hre.ethers.getContract<SafeNFT>('SafeNFT');
-    info(
-      `current presale start date is: ${moment
-        .unix((await nftContract.presaleStartDate()).toNumber())
-        .format(timeStampFormat)}`,
-    );
     if (!test) {
-      if (setdate) {
-        const timeStamp = moment(date, timeStampFormat).unix();
-        info(`setting presale start to ${date}, which is ${timeStamp}`);
-        await (await nftContract.setPresaleStartDate(timeStamp, WEEK(hre.network.name))).wait();
-      }
       if (price) {
-        info(`setting discounted price table start to:`);
-        displayDiscountedPresalePriceNFT(discountedPresalePriceNFT, info);
-        await (await nftContract.setDiscountedPriceTable(discountedPresalePriceNFT.map(o => Object.values(o)))).wait();
         info(`setting presaleMaxSupply to: ${presaleMaxSupply}`);
         await (await nftContract.setPresaleMaxSupply(presaleMaxSupply)).wait();
       } else {
